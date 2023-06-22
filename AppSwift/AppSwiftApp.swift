01c758d5eb565c61570fa986dbdbad40e4abb4a6
popov-va <popov-va@zvuk.com>
2023-06-22 19:13:18 +0300

    commit

---
+++ AppSwift/AppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftApp.swift
+//  AppSwift
+//
+//  Created by popov-va on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
